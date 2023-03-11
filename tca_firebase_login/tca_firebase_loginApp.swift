a6eb2c492c5927ab06a8847c1b962560b7ecab36
shinohara-san <shinohara.yuki.2250@moneyforward.co.jp>
2023-03-11 11:57:51 +0900

    Initial Commit

---
+++ tca_firebase_login/tca_firebase_loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_firebase_loginApp.swift
+//  tca_firebase_login
+//
+//  Created by shinohara.yuki.2250 on 2023/03/11.
+//
+
+import SwiftUI
+
+@main
+struct tca_firebase_loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
